a3e055c5049253683ad97558716b0810575965c6
David Aurelio <david@uxebu.com>
2011-06-17 13:57:44 +0200

    adding `salt.mixin.all`

--- src/mixin.js
+++ src/mixin.js
@@ -1,5 +1,8 @@
 /**
- * Mixes properties of objects into another objects.
+ * Mixes own properties of objects into another objects.
+ *
+ * Only takes own properties into consideration and skips enumerable properties
+ * in the prototype chain.
  *
  * @param {Object} objTo The object to mix properties into.
  * @param {Object} objFrom... Objects to copy properties from.
@@ -12,6 +15,25 @@ salt.mixin = function(objTo, objFrom) {
             if (objFrom.hasOwnProperty(name)) {
                 objTo[name] = objFrom[name];
             }
+        }
+    } while ((objFrom = arguments[i++]));
+    return objTo;
+};
+
+/**
+ * Mixes all properties of objects into another objects.
+ *
+ * Mixes in both own properties and properties in the prototype chain.
+ *
+ * @param {Object} objTo The object to mix properties into.
+ * @param {Object} objFrom... Objects to copy properties from.
+ * @returns {Object} returns `objTo`
+ */
+salt.mixin.all = function(objTo, objFrom) {
+    var i = 2;
+    do {
+        for (var name in objFrom) {
+            objTo[name] = objFrom[name];
         }
     } while ((objFrom = arguments[i++]));
     return objTo;
